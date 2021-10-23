0064efb7152304bf739bb34a54d28624169cebb6
Usama Elsayed <usamaelsayed101@gmail.com>
2021-10-23 15:48:33 +0200

    all done

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// // ul vars
+// const birthdayContainer = document.getElementById("birthday-container");
+
+// const birthdayItem = `<div class="col-md-4 col-lg-3 col-sm-6 col-xl-2">
+// <div class="member-birthday">
+//   <div class="image">
+//     <img src="img/avatars/t1.jpg" class="pb-2" />
+//   </div>
+//   <span class="dob m-0 p-0">15 oct</span>
+//   <div class="story-btn" title="My Story">
+//     <div class="story-btn-line"></div>
+//   </div>
+//   <div class="story">
+//     <p class="story-paragraph">Share your wishes with Monica</p>
+//     <i class="uil uil-envelope"></i>
+//   </div>
+// </div>
+
+// </div>`;
+
+// birthdayContainer.innerHTML = birthdayItem;
